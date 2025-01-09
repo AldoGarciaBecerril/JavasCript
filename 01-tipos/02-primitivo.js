2878388e83eec773101d51511335f8bda55a9ff2
Aldo David Garcia Becerril <jescricdy1@hotmail.com>
2025-01-09 16:49:49 -0600

    Primitivos

---
+++ 01-tipos/02-primitivo.js
@@ -0,0 +1,9 @@
+let nuemro = 1;
+let texto = 'Basic JavaScript';
+let verdadero = true;
+let falso = false;
+let noDefinido;
+let undef = undefined;
+let nulo = null;
+
+console.log(nuemro, texto, verdadero, falso, noDefinido, undef, nulo);
