21a810dd40b7064fc02c0e729dc43f28c8d9a7c8
Aldo David Garcia Becerril <jescricdy1@hotmail.com>
2025-01-30 12:54:51 -0600

    arrays y functions

--- 01-tipos/07-arrays.js
+++ 01-tipos/07-arrays.js
@@ -1,3 +1,14 @@
 let animales = ['cerdo', 'caballo', 'mono', 'tapir'];
 
 console.log(animales);
+console.log(animales[0]);
+
+animales [4] = 'dragon';
+console.log(animales);
+
+animales [11] = 'pez';
+console.log(animales);
+
+console.log(animales[9]);
+
+console.log(typeof animales);
---
+++ 01-tipos/08-funciones.js
@@ -0,0 +1,13 @@
+
+function saludar() {
+    console.log('Buenos dias a todos');
+}
+
+saludar();
+
+function suma() {
+    return 3 + 7;
+}
+
+let resultado = suma();
+console.log(resultado);
